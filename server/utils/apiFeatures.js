433a8bd169c9953e6e54a740fdbab729db9d0cd9
Gustavo Zapata <tavordie@hotmail.com>
2020-02-19 23:23:44 -0800

    cleaning up

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -5,25 +5,20 @@ class APIFeatures {
   }
 
   filter() {
-    //creates a copy of the request.query object e.g. {duration: 7, price: 234}
     const queryObj = { ...this.queryString };
 
-    //we don't want to include these (since they aren't actual properties of the User object)
     const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach(el => delete queryObj[el]); //deletes page, sort, etc from the query object
+    excludedFields.forEach(el => delete queryObj[el]);
 
-    //Advance Filter
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
-    // let query = User.find(JSON.parse(queryStr)); //this is like: find({duration: 7, price: {gte: 100} })
 
     return this;
   }
 
   sort() {
-    //Sort
     if (this.queryString) {
       const sort = this.queryString.sort.split(",").join(" ");
       this.query = this.query.sort(sort); //this is like: sort(price) | from the url: /users?sort=price
@@ -35,7 +30,6 @@ class APIFeatures {
   }
 
   limitFields() {
-    //Fields
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(",").join(" ");
       this.query = this.query.select(fields);
@@ -46,7 +40,6 @@ class APIFeatures {
   }
 
   paginate() {
-    //Pagination
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
