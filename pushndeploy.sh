78ee89a5c3b6e5af656ebf8a6ac59002dbbc2750
Gustavo Zapata <tavordie@hotmail.com>
2020-03-10 01:08:44 +0000

    New Pipeline for Zona!

--- pushndeploy.sh
+++ pushndeploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+addncommit(){
+    git add .
+    git commit -m "$msg"
+}
+
+#project (GitHub)
+echo "Enter commit message:"
+read msg
+addncommit
+echo "\033[1;32mProject (GitHub) repo done \033[0m"
+
+#server (Heroku)
+cd ../../server && addncommit
+echo "\033[1;32mServer (Heroku) repo done \033[0m"
+
 echo "Pushing and deploying app..."
 
 #project (GitHub)
@@ -10,16 +25,12 @@ echo "\033[1;32mProject (GitHub) repo done \033[0m"
 cd client && npm run build
 cd deploy
 shopt -s extglob
-# find . ! -name '.git' ! -name '.gitignore' ! -name '.cpanel.yml' -type f -exec rm -f {} + #delete all but these files
 rm -rf !(.git) #delete all folders but .git
-# rm -r static #delete static folder
 cd .. #go to client
 cp -a ./build/. ./deploy/ #copy all files from build to deploy
-cd deploy && git push
+cd deploy && addncommit
+git push
 echo "\033[1;32mClient (cPanel) repo done \033[0m"
-# "homepage": "https://zona.gustavozapata.me",
-# "predeploy": "npm run build",
-# "deploy": "gh-pages -d build",
 
 #server (Heroku)
 cd ../../server
